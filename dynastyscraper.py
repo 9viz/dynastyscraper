80c35ecddff33a7686650a4c9ff91824fe700103
Visuwesh <visuwesh@tutanota.com>
2021-10-02 16:24:49 +0530

    Rewrite to not use `volp'.
    This was unnecessary.  I created it in when I didn't understand the
    structure of the HTML file.

--- dynastyscraper.py
+++ dynastyscraper.py
@@ -20,9 +20,6 @@ soup = bs4.BeautifulSoup(req.urlopen(base), "html.parser")
 # Chapters.  May or may not be divided by volumes.
 chapters = soup.find("dl", class_="chapter-list")
 
-# Volume?
-volp = bool(chapters.find("dt"))
-
 # dt is usually the volume number.
 # Return a dictionary with url as key, filename as val
 # def get_links(dds, dt=""):
@@ -44,19 +41,16 @@ def print_links(dds, dt=""):
 
     return
 
-if volp:
-    vol = ""
-    dds = []
-    for i in chapters:
-        if not isinstance(i, bs4.element.Tag):
-            continue
+vol = ""
+dds = []
+for i in chapters:
+    if not isinstance(i, bs4.element.Tag):
+        continue
 
-        if i.name == "dt":
-            print_links(dds, vol + "_")
-            vol = i.string
-            dds = []
-        elif i.name == "dd":
-            dds.append(i)
-    if dds: print_links(dds, (vol + "_") if vol else "")
-else:
-    print_links(chapters.find_all("dd"))
+    if i.name == "dt":
+        print_links(dds, vol + "_")
+        vol = i.string
+        dds = []
+    elif i.name == "dd":
+        dds.append(i)
+if dds: print_links(dds, (vol + "_") if vol else "")
