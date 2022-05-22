5a8c3414346d8aaa8a9e5f740bea53e8e78ec0a0
Visuwesh <visuweshm@gmail.com>
2022-05-22 20:49:58 +0530

    * shell.nix: Add duktape and pyduktape to environment

--- shell.nix
+++ shell.nix
@@ -1,10 +1,25 @@
 with import <nixpkgs> {};
 
-stdenv.mkDerivation rec {
+let
+  pyduktape = python3Packages.buildPythonPackage rec {
+    pname = "pyduktape";
+    version = "0.0.6";
+    doCheck = false;
+    propagatedBuildInputs = with python3Packages; [ cython ];
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "1g1k4m5k11nfsnljn5c042zdsbrmv5r4x8g1wkny8mj48jgksqg1";
+    };
+  };
+in stdenv.mkDerivation rec {
   name = "dynastyscraper-environment";
 
   buildInputs = with python3Packages; [
     beautifulsoup4
-  ] ++ [ parallel ];
+  ] ++ [
+    parallel
+    duktape
+    pyduktape
+  ];
   nativeBuildInputs = [];
 }
