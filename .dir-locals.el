23e2ccbc9f5a7589ddc728ae2f459e41717dedb9
Visuwesh <visuweshm@gmail.com>
2022-05-22 20:49:25 +0530

    ; Misc.
    * .dir-locals.el: Unset M-q.
    * .gitignore: Ignore "crypto.js".

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((python-shell-interpreter . "./emacs-python"))))
+((python-mode
+  . ((python-shell-interpreter . "./emacs-python")
+	 ;; `vz/indent-dwim' logic is broken in python-mode buffers.
+	 (eval . (local-unset-key (kbd "M-q"))))))
