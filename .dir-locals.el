8a09f35009431a81e870f96970e711912db07cff
Visuwesh <visuweshm@gmail.com>
2022-06-10 16:30:33 +0530

    ; * Change venv root
    * .dir-locals.el:
    * .gitignore:
    * run (mkdcd): Change venv root.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,6 @@
 
 ((python-mode
   . ((eval . (setq-local python-shell-virtualenv-root
-						 (expand-file-name (project-root (project-current)))))
+						 (expand-file-name ".venv" (project-root (project-current)))))
 	 ;; `vz/indent-dwim' logic is broken in python-mode buffers.
 	 (eval . (local-unset-key (kbd "M-q"))))))
